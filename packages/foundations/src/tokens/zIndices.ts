2915b7f0691bf325c9e310c4dea30f76e0cf6dbd
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-03-14 02:34:56 +0900

    add zindices tokens

---
+++ packages/foundations/src/tokens/zIndices.ts
@@ -0,0 +1,12 @@
+export type ZIndexTokens = {
+  hide?: number;
+  sticky?: number;
+  menu?: number;
+  dropdown?: number;
+  popover?: number;
+  tooltip?: number;
+  overlay?: number;
+  modal?: number;
+  toast?: number;
+  alert?: number;
+};
