5b54eae34d1bd0559da35e07e1d967f836fa6fad
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2026-03-18 01:17:53 +0900

    minor fix

--- apps/tester/src/App.tsx
+++ apps/tester/src/App.tsx
@@ -1,3 +1,14 @@
 export function App() {
-  return <h1>Tester App</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "90vw",
+        height: "90vh",
+      }}
+    >
+      <h1>Tester App</h1>
+    </div>
+  );
 }
