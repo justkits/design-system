8ca664a01c9861794107716471b053189571c753
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-03-31 16:26:15 +0900

    remove old test

--- packages/headless-ui/tests/testcases/Toast/aria.test.tsx
+++ packages/headless-ui/tests/testcases/Toast/aria.test.tsx
@@ -3,26 +3,6 @@ import { render } from "@testing-library/react";
 import { TestComponent } from "./_setup";
 
 describe("Toast - aria", () => {
-  describe("ID", () => {
-    it("Toast.Message's ID matches Toast.Content's aria-describedby", () => {
-      const { getByTestId } = render(<TestComponent isOpen />);
-
-      const content = getByTestId("toast-content");
-      const message = getByTestId("toast-message");
-      expect(message.getAttribute("id")).toBe(
-        content.getAttribute("aria-describedby"),
-      );
-    });
-
-    it("Toast.Content's aria-describedby and aria-description are null if Toast.Message is not rendered", () => {
-      const { getByTestId } = render(<TestComponent isOpen omit="message" />);
-
-      const content = getByTestId("toast-content");
-      expect(content.getAttribute("aria-describedby")).toBeNull();
-      expect(content.getAttribute("aria-description")).toBeNull();
-    });
-  });
-
   describe("Attributes", () => {
     it("Toast.Content always has role='status', aria-atomic='true' and aria-live='polite' by default", () => {
       const { getByTestId } = render(<TestComponent isOpen />);
