ba350346cf9a8fd7591eb15941d5913fb78212e9
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-04-03 15:43:34 +0900

    add modal component and test code

---
+++ packages/headless-ui/src/Modal/Provider.tsx
@@ -0,0 +1,59 @@
+import { type ReactNode, useMemo, useRef, useState } from "react";
+
+import { useClickOutside } from "@/core/clicks/useClickOutside";
+import { useFocusTrap } from "@/core/focus/useFocusTrap";
+import { useOutsideInert } from "@/core/inert/useOutsideInert";
+import { useKeyboardEvent } from "@/core/keyboard/useKeyboardEvent";
+import { useBackgroundScrollLock } from "@/core/scroll-lock/useBackgroundScrollLock";
+import { useOpenState } from "@/core/states/useOpenState";
+import { ModalContext } from "./internals/contexts";
+
+type ModalProps = {
+  children: ReactNode;
+} & (
+  | { isOpen: boolean; onOpenChange: (open: boolean) => void }
+  | { isOpen?: never; onOpenChange?: never }
+);
+
+export function Provider({
+  children,
+  isOpen: controlledOpen,
+  onOpenChange,
+}: ModalProps) {
+  const {
+    isOpen,
+    show: openModal,
+    hide: closeModal,
+  } = useOpenState(controlledOpen, onOpenChange, false);
+  const triggerRef = useRef<HTMLElement | null>(null);
+  const floatingRef = useRef<HTMLDialogElement | null>(null);
+  const [isPending, setPending] = useState(false);
+  const [titleId, setTitleId] = useState<string | undefined>(undefined);
+
+  useClickOutside(floatingRef, closeModal, isOpen, triggerRef);
+  useKeyboardEvent("Escape", closeModal, isOpen);
+  useFocusTrap(floatingRef, isOpen, triggerRef);
+  useBackgroundScrollLock(isOpen);
+  useOutsideInert(floatingRef, isOpen);
+
+  const contextValue = useMemo(
+    () => ({
+      isOpen,
+      openModal: () => openModal(),
+      closeModal: () => closeModal(),
+      isPending,
+      setPending,
+      titleId,
+      setTitleId,
+      triggerRef,
+      floatingRef,
+    }),
+    [isOpen, openModal, closeModal, isPending, titleId],
+  );
+
+  return (
+    <ModalContext.Provider value={contextValue}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
