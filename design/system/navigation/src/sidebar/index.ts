c0dc7276f5e22bc3e9ea6caa1aa7c14af676ad25
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-04-28 18:15:05 +0900

    exports

--- design/system/navigation/src/sidebar/index.ts
+++ design/system/navigation/src/sidebar/index.ts
@@ -1,11 +1,16 @@
-import { SidebarProvider } from "./contexts/Provider";
-
-import { SidebarToggle } from "./toggle/Toggle";
-
-export const Sidebar = Object.assign(SidebarProvider, {
-  Toggle: SidebarToggle,
-});
-
 export { SidebarProvider } from "./contexts/Provider";
 
 export { SidebarToggle } from "./toggle/Toggle";
+
+export { SidebarContent } from "./wrappers/Content";
+export { SidebarMain } from "./wrappers/Main";
+export { SidebarHeader } from "./wrappers/Header";
+export { SidebarFooter } from "./wrappers/Footer";
+
+// Link
+export { Link as SidebarLink } from "./link/Link";
+
+//// Headless components for advanced use cases
+export { SectionLinkContent } from "./section-link/Content";
+export { SectionLinkToggle } from "./section-link/Toggle";
+export { SectionLinkProvider } from "./section-link/Provider";
